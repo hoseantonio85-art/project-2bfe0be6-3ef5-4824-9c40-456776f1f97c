6480e9bde18ba8eb6766869747fcf0e023befa4e
hoseantonio85-art <hoseantonio85@gmail.com>
2026-03-30 10:53:14 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: 'project-2bfe0be6-3ef5-4824-9c40-456776f1f97c',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
